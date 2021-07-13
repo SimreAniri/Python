c42bbba646cd97224c38f12c7d5f7b314491ebd7
SimreAniri <simre_aniri@bk.ru>
2021-07-13 23:29:23 +0300

    Доработан режим запуска в консольном режиме

--- 4.py
+++ 4.py
@@ -2,12 +2,24 @@
 Написать свой модуль utils и перенести в него функцию currency_rates() из предыдущего задания.
 Создать скрипт, в котором импортировать этот модуль и выполнить несколько вызовов функции currency_rates().
 Убедиться, что ничего лишнего не происходит.
+
+*Доработать скрипт: теперь он должен работать и из консоли. Например:
+> python task_4_5.py USD
+75.18, 2020-09-05
 """
 
 import utils
+import sys
 
-print(currency_rates(AUD))
+if sys.argv[1:]:
+    print('\nЗапуск из консоли\n')
+    for el in sys.argv[1:]:
+        date, cur_name, exchange_rate = utils.currency_rates(el)
+        print(f'{cur_name}. Дата: {date}\nКурс перевода в рубли: {exchange_rate}\n')
 
-print(currency_rates(brl))
+else:
+    print(utils.currency_rates('AUD'))
 
-print(currency_rates(Dkk))
+    print(utils.currency_rates('brl'))
+
+    print(utils.currency_rates('Dkk'))
