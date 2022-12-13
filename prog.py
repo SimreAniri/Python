766c4dfcc961312979c9a8ce741141818288530b
SimreAniri <simre_aniri@bk.ru>
2022-12-13 17:19:31 +0300

    Добавление текстовых комментариев в работе программы

--- prog.py
+++ prog.py
@@ -1,9 +1,10 @@
 str_list = input("Введите символы через пробел: ").split()
-print(str_list)
+#print(str_list)
 
 new_str = []
 for s in str_list:
     if len(s) <= 3:
         new_str.append(s)
 
+print("Массив строк длиной не больше 3 символов:")
 print(new_str)
